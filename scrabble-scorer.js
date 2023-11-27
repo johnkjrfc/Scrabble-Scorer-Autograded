aa59d2a3fb9429bf96dd31bc3eb4f61dce657bdf
johnkjrfc <johnkjrfc@gmail.com>
2023-11-27 12:09:31 -0600

    Up to part 4

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -85,40 +85,17 @@ function scorerPrompt() {
       console.log(scoringAlgorithms[i]);
    }
    let userAlgorithm = input.question(`Please enter 0 for Simple Score, 1 for Bonus Vowels, or 2 for Scrabble :`);
-   return scoringAlgorithms[userAlgorithm]
+   return scoringAlgorithms[userAlgorithm];
 }
 
-function transform() {};
+function transform(oldPointStructure) {
+   const newPointStructure = {};
 
-let newPointStructure = {
-   A: 1,
-   B: 3,
-   C: 3,
-   D: 2,
-   E: 1,
-   F: 4,
-   G: 2,
-   H: 4, 
-   I: 1, 
-   J: 8, 
-   K: 5, 
-   L: 1, 
-   M: 3, 
-   N: 1, 
-   O: 1, 
-   P: 3, 
-   Q: 10, 
-   R: 1, 
-   S: 1, 
-   T: 1, 
-   U: 1, 
-   V: 4, 
-   W: 4, 
-   X: 8, 
-   Y: 4, 
-   Z: 10, 
+
 
 };
+
+let newPointStructure ;
 
 function runProgram() {
    let word = initialPrompt();
